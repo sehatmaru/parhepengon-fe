5e4c583dec548c29899004461d8431161f07a282
sehatmaru <sehat.samosir@adapundi.com>
2023-06-21 17:07:39 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ClipboardModule } from 'ngx-clipboard';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,34 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    // AuthModule,
+    // DashboardModule,
+    // SharedModule,
+    ClipboardModule,
+    RouterModule.forRoot([]),
+    NgbModule,
+    // ServiceWorkerModule.register('ngsw-worker.js', {
+    //   enabled: !isDevMode(),
+    //   // Register the ServiceWorker as soon as the application is stable
+    //   // or after 30 seconds (whichever comes first).
+    //   registrationStrategy: 'registerWhenStable:30000'
+    // }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    // AuthGuardService, 
+    // ProfileGuardService,
+    // OtpGuardService,
+    // ForgotGuardService,
+    // {
+    //   provide: HTTP_INTERCEPTORS,
+    //   useClass: CommonInterceptor,
+    //   multi: true
+    // }
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    // LoaderComponent
+  ]
 })
 export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  baseURL: 'https://api-xcode-parhepengon.up.railway.app/',
+  tinyMCEApiKey: 'ihft6sbfszrip7c3uiag2m08nc0q7vm20bbrv4idta5lci2x'
+};
